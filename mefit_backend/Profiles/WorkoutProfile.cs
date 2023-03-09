f13e69590007fbe5e80efbcc39fce917ab303cea
AdamNymanExperis <122439687+AdamNymanExperis@users.noreply.github.com>
2023-03-09 16:55:56 +0100

    feat: added Profile for workout

---
+++ mefit_backend/Profiles/WorkoutProfile.cs
@@ -0,0 +1,23 @@
+﻿using mefit_backend.models.domain;
+using mefit_backend.Models.DTO.WorkoutDtos;
+using Microsoft.Extensions.Options;
+using AutoMapper;
+
+namespace mefit_backend.Profiles
+{
+    public class WorkoutProfile: AutoMapper.Profile
+    {
+        public WorkoutProfile()
+        {
+            CreateMap<AddWorkoutDTO, Workout>();
+            CreateMap<PutWorkoutDTO, Workout>();
+            CreateMap<Workout, GetWorkoutDTO>()
+                .ForMember(dto => dto.WorkoutGoals, options =>
+                options.MapFrom(workoutDomain => workoutDomain.WorkoutGoals.Select(goal => $"api/v1/Goal/{goal.Id}").ToList()))
+                .ForMember(dto => dto.WorkoutExercises, options =>
+                options.MapFrom(workoutDomain => workoutDomain.WorkoutExercises.Select(workout => $"api/v1/Workout/{workout.Id}").ToList()))
+                .ForMember(dto => dto.FitnessPrograms, options =>
+                options.MapFrom(workoutDomain => workoutDomain.FitnessPrograms.Select(fitnessprogram => $"api/v1/FitnessProgram/{fitnessprogram.Id}").ToList()));
+        }
+    }
+}
